1910275927830de2f4b499c4e4dabf68d93238e7
Aditya Gupta <adityagupta1089@gmail.com>
2018-04-27 22:29:23 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+
+<head>
+    <title>
+        CSL301 Project!
+    </title>
+</head>
+
+<body>
+    Hello World!
+    <ul>
+        <li>
+            Aditya Gupta
+        </li>
+        <li>
+            Vinit Kothawade
+        </li>
+        <li>
+            Shreya dubey
+        </li>
+    </ul>
+</body>
+
+</html>
