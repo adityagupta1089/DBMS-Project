33d4576c61919527c74beb206237c6a1ee516fe6
Aditya <adityagupta1089@gmail.com>
2018-04-29 21:43:34 +0530

    Fixed Login and made portals for 3 parties.

--- index.php
+++ index.php
@@ -6,20 +6,22 @@
 
     if($_SERVER["REQUEST_METHOD"] == "POST") {
         
-        $username = mysqli_real_escape_string($db, $_POST['uesrname']);
+        $username = mysqli_real_escape_string($db, $_POST['username']);
         
-        $password = mysqli_real_escape_string($db, $_POST['password']);
+        $password = mysqli_real_escape_string($db, $_POST['password']);  
         
         $sql = "SELECT id FROM admin WHERE username = '$username' and password = '$password'";
-        $result = mysqli_query($db, $sql);
+
+        $result = mysqli_query($db, $sql);        
         $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-        $count = mysqli_num_rows($result);
+        $count = mysqli_num_rows($result);  
         
         if ($count == 1) {
-            session_register("username");
-            $_SESSION['login_user'] = $username;
-            
-            header("location:welcome.php");
+            $_SESSION['login_user'] = $username;    
+            $_SESSION['position'] = $row["id"];
+            if ($_SESSION['position'] == 0) {
+                header("location:students.php");
+            }
         } else {
             $error = "Your Login Name or Password is invalid";
         }
@@ -56,15 +58,15 @@
             <!-- Login Form -->
             <div id="login" class="tab-pane active container">
                 <!-- Login form -->
-                <form id="login-form">
+                <form id="login-form" action="" method="post">
                     <div>
                         <div class="form-group">
-                            <label for="login-name">User name</label>
-                            <input type="text" class="form-control" id="login-name" placeholder="Username">
+                            <label for="username">User name</label>
+                            <input type="text" name="username" class="form-control" id="username" placeholder="Username">
                         </div>
                         <div class="form-group">
-                            <label for="login-pass">Password</label>
-                            <input type="password" class="form-control" id="login-pass" placeholder="Password">
+                            <label for="password">Password</label>
+                            <input type="password" name="password" class="form-control" id="password" placeholder="Password">
                         </div>
                         <div style="font-size:11px; color:#cc0000; margin-top:10px">
                             <?php echo $error; ?>
@@ -76,12 +78,13 @@
                 </form>
             </div>
 
-
-            <!-- Bootstap core Javascripts -->
-            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
         </div>
+        
+        <!-- Bootstap core Javascripts -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+
     </body>
 
     </html>
