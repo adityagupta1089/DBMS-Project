d0a883ac10621ebd1712e631fbe43ca124f2088b
vinit-k <2015csb1039@iitrpr.ac.in>
2018-04-29 21:56:51 +0530

    fixed index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         
         $password = mysqli_real_escape_string($db, $_POST['password']);  
         
-        $sql = "SELECT id FROM admin WHERE username = '$username' and password = '$password'";
+        $sql = "SELECT category FROM admin WHERE username = '$username' and password = '$password'";
 
         $result = mysqli_query($db, $sql);        
         $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
@@ -18,7 +18,7 @@
         
         if ($count == 1) {
             $_SESSION['login_user'] = $username;    
-            $_SESSION['position'] = $row["id"];
+            $_SESSION['position'] = $row["category"];
             if ($_SESSION['position'] == 0) {
                 header("location:students.php");
             } elseif ($_SESSION['position'] == 1){
