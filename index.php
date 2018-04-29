c88369f00c0ed794925d765f772d0568ca4447aa
Aditya <adityagupta1089@gmail.com>
2018-04-29 23:42:28 +0530

    Removed styling, updated students.php

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
     include("config.php");
     session_start();
 
+    if(isset($_SESSION['login_user'])){
+      header("location:redirect.php");
+   }
+
     if($_SERVER["REQUEST_METHOD"] == "POST") {
         
         $username = mysqli_real_escape_string($db, $_POST['username']);
@@ -20,13 +24,7 @@
             $_SESSION['login_user'] = $username;    
             $_SESSION['position'] = $row["category"];
             $_SESSION['id'] = $row["id"];
-            if ($_SESSION['position'] == 0) {
-                header("location:students.php");
-            } elseif ($_SESSION['position'] == 1){
-                header("location:teachers.php");
-            } elseif ($_SESSION['position'] == 2) {
-                header("location:staff.php");
-            }
+            header("location:redirect.php");
         } else {
             $error = "Your Login Name or Password is invalid";
         }
@@ -46,49 +44,26 @@
             CSL301 Project!
         </title>
 
-        <!-- Bootstrap core CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
-
-        <!-- Custom styles -->
-        <link rel="stylesheet" type="text/css" href="css/index.css">
-
     </head>
 
     <body>
-        <div class="container">
+        <!-- Title -->
+        <h1 class>Login into Academic Portal</h1>
 
-            <!-- Title -->
-            <h1 class="display-2 text-center">Login into Academic Portal</h1>
-
-            <!-- Login Form -->
-            <div id="login" class="tab-pane active container">
-                <!-- Login form -->
-                <form id="login-form" action="" method="post">
-                    <div>
-                        <div class="form-group">
-                            <label for="username">Username</label>
-                            <input type="text" name="username" class="form-control" id="username" placeholder="Username">
-                        </div>
-                        <div class="form-group">
-                            <label for="password">Password</label>
-                            <input type="password" name="password" class="form-control" id="password" placeholder="Password">
-                        </div>
-                        <div style="font-size:11px; color:#cc0000; margin-top:10px">
-                            <?php echo $error; ?>
-                        </div>
-                        <div class="row">
-                            <button class="btn btn-primary" type="submit">Login</button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-
+        <!-- Login Form -->
+        <div id="login">
+            <!-- Login form -->
+            <form id="login-form" action="" method="post">
+                <div>
+                    <label for="username">Username</label>
+                    <input type="text" name="username" class="form-control" id="username" placeholder="Username">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" class="form-control" id="password" placeholder="Password">
+                    <?php echo $error; ?>
+                    <button type="submit">Login</button>
+                </div>
+            </form>
         </div>
-        
-        <!-- Bootstap core Javascripts -->
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
 
     </body>
 
