e2bb767274fcaf7d7ddb0a8d855c77faf98f213e
Aditya <adityagupta1089@gmail.com>
2018-04-30 15:24:18 +0530

    Completed students.php

--- students.php
+++ students.php
@@ -43,27 +43,22 @@
 
             <div id="acadperf">
                 <h1>Academic Performance</h1>
+                <h2><a href=".">Refresh</a></h2>
                 <table border=1>
                     <thead>
                         <tr>
                             <th>Course ID</th>
                             <th>Minimum CGPA</th>
-                            <th>Grade</th>
                         </tr>
                     </thead>
                     <tbody>
                         <?php
-                                $sql = "SELECT offers.course_id, offers.minimum_cgpa, takes.grade FROM takes, offers WHERE offers.offer_id = takes.offer_id AND takes.student_id =" .  $_SESSION["id"];
+                                $sql = "SELECT offers.course_id, offers.minimum_cgpa FROM takes, offers WHERE offers.offer_id = takes.offer_id AND takes.student_id = " .  $_SESSION["id"];
                                 $result = mysqli_query($db, $sql);                            
                                 while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
                                     echo "<tr>";
                                     echo '<th scope="row">' . $row["course_id"] . "</th>";
                                     echo '<td>' . $row["minimum_cgpa"] . '</td>';
-                                    if ($row["grade"]) {
-                                        echo '<td>' . $row["grade"] . '</td>';
-                                    } else {
-                                        echo '<td>Not Yet Completed</td>';
-                                    }
                                     echo "</tr>";
                                 }
                     ?>
@@ -75,23 +70,74 @@
             <div id="regcourse">
                 <h1>Register for a course</h1>
                 Select Course:
-                <select>
+                <?php
+                    if(isset($_POST['register'])){
+                        $oid = $_POST['course_register'];  
+                    } else {
+                        $oid = -1;
+                    }
+                ?>
+                    <form action="#" method="post">
+                        <select name="course_register">
                             <?php
-                                $sql = "SELECT offers.course_id, courses.name FROM offers, courses WHERE offers.course_id = courses.course_id";
+                                $sql = "SELECT offers.offer_id, offers.course_id, courses.name FROM offers, courses WHERE offers.course_id = courses.course_id";
                                 $result = mysqli_query($db, $sql);                            
                                 while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-                                    echo "<option><a href=\"#\" class=\"dropdown-item\">";
-                                    echo $row["course_id"] . ": " . $row["name"];
+                                    echo "<option value=\"" . $row["offer_id"] ."\"";
+                                    if ($oid == $row["offer_id"]) echo "selected";
+                                    echo "><a href=\"#\" class=\"dropdown-item\">";
+                                    echo "Offer #" . $row["offer_id"] . ":   " . $row["course_id"] . "(" . $row["name"] . ")";
                                     echo "</a></option>";
                                 }
+                            ?>
+                        </select>
+                        <input type="submit" name="register" value="Register" />
+                    </form>
+                    <?php
+                        if (isset($_POST['register'])){
+                            $sql = "INSERT INTO Takes(offer_id, student_id) VALUES (" . $oid . "," . $_SESSION["id"] . ")";
+                            $result = mysqli_query($db, $sql);
+                            if ($result) {
+                                echo "Course successfully registered";
+                            } else {
+                                echo "Error registering course: " . mysqli_error($db) . " (Try generating a aticket)";
+                            }
+                        }
                     ?>
-                </select>
-                <button>Register</button>
+
             </div>
 
             <div id="genticket">
                 <h1>Generate a ticket.</h1>
             </div>
+            <form action="#" method="post">
+                <select name="ticket">
+                    <?php
+                        $sql = "SELECT offers.offer_id, offers.course_id, courses.name FROM offers, courses WHERE offers.course_id = courses.course_id";
+                        $result = mysqli_query($db, $sql);                            
+                        while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+                            echo "<option value=\"" . $row["offer_id"] ."\"";
+                            if ($oid == $row["offer_id"]) echo "selected";
+                            echo "><a href=\"#\" class=\"dropdown-item\">";
+                            echo "Offer #" . $row["offer_id"] . ":   " . $row["course_id"] . "(" . $row["name"] . ")";
+                            echo "</a></option>";
+                        }
+                    ?>
+                </select><br>
+                <textarea name="comment" form="usrform" placeholder="Comments" style="margin-top:20px;width:400px;height:70px;"></textarea><br>
+                <input type="submit" name="ticket-submit" value="Submit Ticket" style="margin-top:20px;"/>
+            </form>
+            <?php
+                if (isset($_POST['ticket-submit'])) {
+                    $sql = "INSERT INTO Tickets(Offer_ID, Student_ID, Status) VALUES (" . $oid . "," . $_SESSION["id"] . ", 0)";
+                    $result = mysqli_query($db, $sql);
+                    if ($result) {
+                        echo "Ticket successfully submitted";
+                    } else {
+                        echo "Error submitting ticket";
+                    }
+                }
+            ?>
 
         </div>
     </body>
